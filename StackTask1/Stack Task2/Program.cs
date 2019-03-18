c57941c6b579ede6a4b60b58d6865ce0b21bffc9
Damien Haupt <30006071@student.toiohomai.ac.nz>
2019-03-18 16:31:50 +1300

    Done

---
+++ StackTask1/Stack Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contains = "";
+            Console.WriteLine("Enter values for the stack/n");
+            Stack s1 = new Stack();
+            Console.WriteLine("Enter Stack number value");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("Enter next Stack number value");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("Enter next Stack number value");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("Enter next Stack number value");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("Enter next Stack number value");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("Enter a value to see if it is in the stack");
+            contains = Console.ReadLine();
+
+
+            if (s1.Contains(contains) == true)
+            {
+                while (s1.Peek().ToString() != contains)
+                {
+                    s1.Pop();
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Does not contain " + contains);
+            }
+            Console.WriteLine($"Contains: {s1.Peek()}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
+    }
+}
---
+++ StackTask1/StackTask1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contains = "";
+            Console.WriteLine("Enter values for the stack/n");
+            Stack s1 = new Stack();
+            Console.WriteLine("Enter Stack number value");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("Enter next Stack number value");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("Enter next Stack number value");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("Enter next Stack number value");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("Enter next Stack number value");
+            s1.Push(Console.ReadLine());
+            
+
+
+            Console.WriteLine(s1.Pop());
+            Console.WriteLine(s1.Pop());
+            Console.WriteLine(s1.Pop());
+            Console.WriteLine(s1.Pop());
+            Console.WriteLine(s1.Pop());
+            Console.ReadLine();
+        }
+    }
+}
